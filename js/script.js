6f5a9e2ec4b7b705548b2391baca1e51e91be759
puhrome <phirom.yim@fullsail.edu>
2014-04-11 22:43:21 -0400

    Slice of Pie Pt I
    Using WebStorm to write expression for pizza equation with HTML and JS.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/**
+ * Slice of Pie Pt I Created by Phirom Yim on 4/11/14.
+ */
+var slices;//declare slices
+slices = 9;//define number of slices
+var people;//declare people
+people = 17;//define number of people at the party
+var pies;//declare pies
+pies = 3;//define number of pies
+//number of pies times number of slices divided by person
+var party; //declare party
+ party = pies * slices;
+ //define party is equal to number of pies divided times number of slices
+var goers; //declare goers
+goers = party/people; //define goers is equal to party divided people
+console.log(goers);
+alert(goers);
+
+
